e3ce0ef9cf1036b3812b29e26a2a3f950384cb6f
Andrei Pit <charmpitz@gmail.com>
2016-07-14 02:04:31 +0300

    added color variables

--- bash_bootstrap.sh
+++ bash_bootstrap.sh
@@ -1,4 +1,17 @@
+# Paths
 CONFIG_PATH=`dirname "$BASH_SOURCE"`
+
+# Colors
+black=$(tput -Txterm setaf 0)
+red=$(tput -Txterm setaf 1)
+green=$(tput -Txterm setaf 2)
+yellow=$(tput -Txterm setaf 3)
+dk_blue=$(tput -Txterm setaf 4)
+pink=$(tput -Txterm setaf 5)
+lt_blue=$(tput -Txterm setaf 6)
+
+bold=$(tput -Txterm bold)
+reset=$(tput -Txterm sgr0)
 
 # Include variables and custom configs
 source $CONFIG_PATH/bash_config.sh
