c376550b86bca645108ad923707dfffde1064868
Andrei Pit <charmpitz@gmail.com>
2016-07-14 01:48:51 +0300

    small fixes

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -9,3 +9,25 @@ alias create_shortcut="gnome-desktop-item-edit ~/.local/share/applications --cre
 
 # Alternative to `tail -f`
 alias log="${CONFIG_PATH}/lnav/lnav -c ":enable-word-wrap" $@"
+
+# Colored and classified
+alias ls='ls -F --color=always'
+
+# Colored and classified
+alias dir='dir -F --color=always'
+
+# Detailed
+alias ll='ls -l'
+
+# Prompt and verbose
+alias cp='cp -iv'
+
+# Prompt
+alias rm='rm -i'
+
+# Prompt and verbose
+alias mv='mv -iv'
+
+# Colored, ignore case and show line number by default
+alias grep='grep --color=auto -in'
+
--- bash_bootstrap.sh
+++ bash_bootstrap.sh
@@ -5,3 +5,9 @@ source $CONFIG_PATH/bash_config.sh
 
 # Include custom aliases
 source $CONFIG_PATH/bash_aliases.sh
+
+# Include system bash completion
+[ -r /etc/bash_completion ] && source /etc/bash_completion
+
+# Include git completion
+[ -r $CONFIG_PATH/git_completion.bash ] && source $CONFIG_PATH/git_completion.bash
