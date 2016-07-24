e0bfcf79b4b87551a990553f1cb40d471b5504bd
Andrei Pit <charmpitz@gmail.com>
2016-07-24 04:00:28 +0300

    added autocomplete wrapper function to use with aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -4,11 +4,17 @@ alias ..="cd .."
 # Up 2 levels
 alias ...="cd ../../"
 
+# Go to ~/workspace
+alias work="cd ~/workspace"
+
+# Go to /var/www
+alias www="cd /var/www"
+
 # Command to open the create shortcut window
 alias create_shortcut="gnome-desktop-item-edit ~/.local/share/applications --create-new"
 
 # Alternative to `tail -f`
-alias log="${CONFIG_PATH}/lnav/lnav -c ":enable-word-wrap" $@"
+alias lnav="${CONFIG_PATH}/lnav/lnav -c ":enable-word-wrap" $@"
 
 # Colored and classified
 alias ls='ls -F --color=always'
@@ -34,10 +40,15 @@ alias grep='grep --color=auto -in'
 # Edit
 alias E='sudo nano $@'
 
-# Install and automplete for it
+# Install and automplete
 alias I='sudo apt-get install $@'
 complete -F _get_packages_autocomplete I
 
-# Remove and autocomplete for it
+# Remove and autocomplete
 alias R='sudo apt-get remove --purge $@'
 complete -F _get_packages_autocomplete R
+
+# Vagrant alias
+alias v='vagrant $@'
+make_completion_wrapper _vagrant _get_vagrant_autocomplete vagrant
+complete -F _get_vagrant_autocomplete v
